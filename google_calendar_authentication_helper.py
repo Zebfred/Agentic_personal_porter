5a924366b791d3b6c460e0a693421734d2e66da8
Zebfred <zebfred22@gmail.com>
2025-09-26 13:33:31 -0500

    debugging neo4j_db, starting google_calendar intergration

---
+++ google_calendar_authentication_helper.py
@@ -0,0 +1,43 @@
+import os
+import pickle
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# This defines what our app is allowed to do. We're asking for read-only access.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+CREDENTIALS_FILE = 'credentials.json'
+TOKEN_PICKLE_FILE = 'token.pickle'
+
+def get_calendar_service():
+    """
+    Handles the OAuth 2.0 flow and returns a service object to interact with the API.
+    It cleverly caches your credentials in a 'token.pickle' file so you don't have
+    to log in every single time you start the server. It's like a bouncer
+    who remembers your face after you show your ID once.
+    """
+    creds = None
+    # Check if we have a saved token (a hall pass from a previous login)
+    if os.path.exists(TOKEN_PICKLE_FILE):
+        with open(TOKEN_PICKLE_FILE, 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there's no valid token, we need to get a new one.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            # If the hall pass is expired, we can refresh it without bothering the user.
+            creds.refresh(Request())
+        else:
+            # This is the one-time-only process where you have to grant permission.
+            # It will print a URL in your Flask terminal. You must copy/paste it into your browser.
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
+            # We use run_console() because our web server isn't a traditional desktop app.
+            creds = flow.run_console()
+
+        # Save the new, shiny token for next time.
+        with open(TOKEN_PICKLE_FILE, 'wb') as token:
+            pickle.dump(creds, token)
+
+    # Build the service object that can actually make API calls.
+    service = build('calendar', 'v3', credentials=creds)
+    return service
